f2ac4cbf7392dc2fb6eb708cf66785827c5c57a8
Falbathe1 <f.albathe@sanaam.sa>
2024-08-22 13:08:47 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,16 +5,14 @@ export type ConfigProps = {
   appDescription: string;
   appName: string;
   domainName: string;
-  appImage: string;
-  logo: FC<SVGProps<SVGSVGElement>>;  // Update this to the correct type for a React component
+  logo: FC<SVGProps<SVGSVGElement>>;  // Correctly typed as a React component
 };
 
-const config = {
+const config: ConfigProps = {
   appName: 'sanaam',
   appDescription: 'We partner with organizations to deliver innovative UI/UX design and effective growth strategies, helping you enhance customer experiences and achieve measurable growth.',
   domainName: 'https://sanaam.sa',
-  appImage: '/icons/logo.svg',
-  logo: Logo,  // This is now a React component
-} as ConfigProps;
+  logo: Logo,  // This is correctly set as a React component
+};
 
 export default config;
