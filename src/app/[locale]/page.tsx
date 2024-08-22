a345e1487dcd6a34c150319b7944d8f10248b476
harzeezco <wakilabdulazeez@gmail.com>
2024-08-22 19:00:47 +0100

    chore: change meta data

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -17,6 +17,9 @@ export const metadata = getSEOTags({
     'We partner with organizations to deliver innovative UI/UX design and effective growth strategies, helping you enhance customer experiences and achieve measurable growth',
   keywords:
     'UI/UX design, Building MVPs, Design & Research, Innovation Lab-aas',
+  twitter: {
+    card: 'summary_large_image',
+  },
 });
 
 const Homepage = () => (
