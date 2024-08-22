0e94c570dd16d3e6770392f6050bfb7c7a66da3e
Falbathe1 <f.albathe@sanaam.sa>
2024-08-22 16:24:05 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,31 +1,13 @@
-import Image from 'next/image';
-
 export type ConfigProps = {
   appDescription: string;
   appName: string;
   domainName: string;
 };
 
-export const config: ConfigProps = {
+const config = {
   appName: 'sanaam',
-  appDescription: '.We partner with organizations to deliver innovative UI/UX design and effective growth strategies, helping you enhance customer experiences and achieve measurable growth.',
-  domainName: 'https://sanaam.sa',
-};
-
-export const MyComponent = () => {
-  return (
-    <div>
-      <h1>{config.appName}</h1>
-      <p>{config.appDescription}</p>
-      <a href={config.domainName}>Visit Us</a>
-      <Image
-        alt="logo"
-        src="/icons/logo.svg"  // Correct path to your logo
-        width={70}
-        height={40}
-      />
-    </div>
-  );
-};
+  appDescription: 'We partner with organizations to deliver innovative UI/UX design and effective growth strategies, helping you enhance customer experiences and achieve measurable growth.',
+  domainName: 'sanaam.sa',
+} as ConfigProps;
 
 export default config;
