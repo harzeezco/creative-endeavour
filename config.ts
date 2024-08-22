9ae667dae6c2981ace058641e134015697da9ede
Falbathe1 <f.albathe@sanaam.sa>
2024-08-22 12:54:32 +0300

    add logo config.ts

--- config.ts
+++ config.ts
@@ -2,12 +2,15 @@ export type ConfigProps = {
   appDescription: string;
   appName: string;
   domainName: string;
+  appImage: string; 
+
 };
 
 const config = {
   appName: 'sanaam',
   appDescription: 'We partner with organizations to deliver innovative UI/UX design and effective growth strategies, helping you enhance customer experiences and achieve measurable growth.',
   domainName: 'sanaam.sa',
+  appImage: '/logo.svg',
 } as ConfigProps;
 
 export default config;
